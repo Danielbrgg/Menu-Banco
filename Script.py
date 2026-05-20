42f3604e4f3979ecfab6690ac44e4fd0c094097b
Daniel Braga <danielbragagomes2022gmail.com>
2026-05-20 10:50:38 -0300

    Ajustes Finais

--- Script.py
+++ Script.py
@@ -1,48 +1,55 @@
 senha = "1234"
 contador = 3
-Saldo = 0
+Saldo = 0.0
 
 while contador > 0:
-        tentativa = input("Digite a sua senha! ")
-        if tentativa != senha:
-            print("SENHA INVÁLIDA")
-            contador -= 1
-            if contador == 0:
-                print("O SISTEMA ENCERROU POR EXCESSO DE 3(TRÊS) TENTATIVAS DE ACESSO INVÁLIDOS")
-                break
-        elif tentativa == senha:
-            print("\n Acesso Permitido")
+    tentativa = input("Digite a sua senha! ")
+    
+    if tentativa != senha:
+        print("SENHA INVÁLIDA")
+        contador -= 1
+        if contador == 0:
+            print("O SISTEMA ENCERROU POR EXCESSO DE 3(TRÊS) TENTATIVAS DE ACESSO INVÁLIDOS")
+            break
             
-            while True:
-                print("\n--- MENU ---")
-                print("1 - Ver Saldo")
-                print("2 - Depósito")
-                print("3 - Saque")
-                print("0 - Sair")
+    elif tentativa == senha:
+        print("\n Acesso Permitido")
+        
+        while True:
+            print("\n--- MENU ---")
+            print("1 - Ver Saldo")
+            print("2 - Depósito")
+            print("3 - Saque")
+            print("0 - Sair")
+                
+            op = input("Digite um numero entre 0-3 \n")
+     
+            match(op):
+                case "1":
+                    print(f"Seu Saldo bancário é de: R${Saldo:.2f}")
+                
+                case "2":
+                    deposito = float(input("Deseja fazer o depósito de quanto: R$"))
+                    if deposito > 0:
+                        Saldo += deposito
+                        print("DEPÓSITO REALIZADO COM SUCESSO")
+                    else:
+                        print("Digite um número positivo")
                     
-                op = int(input("Digite um numero entre 0-3 \n"))
-         
-                match(op):
-                    case "1":
-                        print(f"Seu Saldo bancário é de: R${Saldo:.2f}")
-                    
-                    case "2":
-                        deposito = float(input("Deseja fazer o depósito de quanto: R$"))
-                        if deposito > 0:
-                            Saldo += deposito
-                            print("DEPÓSITO REALIZADO COM SUCESSO")
-                        else:
-                            print("Digite um número positivo")
-                        
-                    case "3":
-                        Saque = float(input("Deseja sacar quanto: R$"))
-                        if Saque > Saldo:
-                            print("SALDO INSUFICIENTE PARA SAQUE")
-                        elif Saque > 0:
-                            Saldo += Saque
-                            print("DEPÓSITO REALIZADO COM SUCESSO")
-                        else:
-                            print("Ocorreu um erro tente Novamente")
-                                    
-                    case "0":
-                        break
+                case "3":
+                    Saque = float(input("Deseja sacar quanto: R$"))
+                    if Saque > Saldo:
+                        print("SALDO INSUFICIENTE PARA SAQUE")
+                    elif Saque > 0:
+                        Saldo -= Saque 
+                        print("SAQUE REALIZADO COM SUCESSO") 
+                    else:
+                        print("Ocorreu um erro tente Novamente")
+                                
+                case "0":
+                    print("Saindo do menu...")
+                    break 
+                
+                case _:
+                    print("Opção inválida!")
+                    break
